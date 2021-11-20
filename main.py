02086e7f70055348513e160d0dc6f6b95d83d432
ivan3L <ivanluiguilopezlopez@gmail.com>
2021-11-20 18:56:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer
+import numpy
+import tflearn
+import tensorflow
+import json
+import random
+import pickle
+
