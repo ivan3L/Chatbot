07345b51f08fe02d61716a86ed19b5c4c627818f
ivan3L <ivanluiguilopezlopez@gmail.com>
2021-11-22 01:36:15 -0500

    updated answers and exceptions

--- main.py
+++ main.py
@@ -18,62 +18,54 @@ from tkinter import *
 with open("answers.json", encoding='utf-8') as file:
     datos = json.load(file)
 
-try:
-    with open("variables.pickle","rb") as pickleFile:
-        palabras, tags, entrenamiento, salida = pickle.load(pickleFile)
+palabras = []
+tags = []
+auxX = []
+auxY = []
 
-except:
-    palabras = []
-    tags = []
-    auxX = []
-    auxY = []
+#Usamos librería nltk para utilizar lenguaje natural y separar palabras
+for content in datos["content"]:
+    for patterns in content["patterns"]:
+        #Agregamos a lista de palabras clave
+        auxWord = nltk.word_tokenize(patterns)
+        #Agregamos a lista de frases
+        palabras.extend(auxWord)
+        #Agregamos a lista de palabras
+        auxX.append(auxWord)
+        #Agregamos a lista de tags
+        auxY.append(content["tag"])
 
-    #Usamos librería nltk para utilizar lenguaje natural y separar palabras
-    for content in datos["content"]:
-        for patterns in content["patterns"]:
-            #Agregamos a lista de palabras clave
-            auxWord = nltk.word_tokenize(patterns)
-            #Agregamos a lista de frases
-            palabras.extend(auxWord)
-            #Agregamos a lista de palabras
-            auxX.append(auxWord)
-            #Agregamos a lista de tags
-            auxY.append(content["tag"])
+        #Añadimos el tag si es que no esta
+        if content["tag"] not in tags:
+            tags.append(content["tag"])
 
-            #Añadimos el tag si es que no esta
-            if content["tag"] not in tags:
-                tags.append(content["tag"])
+palabras = [stemmer.stem(w.lower()) for w in palabras if w!="?"]
+palabras = sorted(list(set(palabras)))
+tags = sorted(tags)
 
-    palabras = [stemmer.stem(w.lower()) for w in palabras if w!="?"]
-    palabras = sorted(list(set(palabras)))
-    tags = sorted(tags)
+entrenamiento = []
+salida = []
 
-    entrenamiento = []
-    salida = []
+salidaVacia = [0 for _ in range(len(tags))]
 
-    salidaVacia = [0 for _ in range(len(tags))]
+for x, documento in enumerate(auxX):
+    cubeta = []
+    auxPalabra = [stemmer.stem(w.lower()) for w in documento]
+    for w in palabras:
+        if w in auxPalabra:
+            cubeta.append(1)
+        else:
+            cubeta.append(0)
+    filaSalida = salidaVacia[:]
+    filaSalida[tags.index(auxY[x])] = 1
+    entrenamiento.append(cubeta)
+    salida.append(filaSalida)
 
-    for x, documento in enumerate(auxX):
-        cubeta = []
-        auxPalabra = [stemmer.stem(w.lower()) for w in documento]
-        for w in palabras:
-            if w in auxPalabra:
-                cubeta.append(1)
-            else:
-                cubeta.append(0)
-        filaSalida = salidaVacia[:]
-        filaSalida[tags.index(auxY[x])] = 1
-        entrenamiento.append(cubeta)
-        salida.append(filaSalida)
+entrenamiento = numpy.array(entrenamiento)
+salida = numpy.array(salida)
 
-    #print(entrenamiento)
-    #print(salida)
-
-    entrenamiento = numpy.array(entrenamiento)
-    salida = numpy.array(salida)
-
-    with open("variables.pickle","wb") as pickleFile:
-        pickle.dump((palabras,tags,entrenamiento,salida),pickleFile)
+with open("variables.pickle","wb") as pickleFile:
+    pickle.dump((palabras,tags,entrenamiento,salida),pickleFile)
 
 tensorflow.compat.v1.reset_default_graph()
 
@@ -87,13 +79,10 @@ red = tflearn.regression(red)
 
 modelo = tflearn.DNN(red)
 
-try:
-    modelo.load("modelo.tflearn")
-except:
-    modelo.fit(entrenamiento,salida,n_epoch=1000,batch_size=100,show_metric=True)
-    modelo.save("modelo.tflearn")
+modelo.fit(entrenamiento,salida,n_epoch=1000,batch_size=10,show_metric=True)
+modelo.save("modelo.tflearn")
 
-def mainBot(entrada):
+def botAnswer(entrada):
     cubeta = [0 for _ in range(len(palabras))]
     entradaProcesada = nltk.word_tokenize(entrada)
     entradaProcesada = [stemmer.stem(palabra.lower()) for palabra in entradaProcesada]
@@ -115,7 +104,7 @@ def mainBot(entrada):
 
 def send():
     input = e.get()
-    rpta = mainBot(input)
+    rpta = botAnswer(input)
     txt.insert(END,"\n"+"Tú: "+input)
     e.delete(0,END)
     txt.insert(END,"\n"+"Washi Bot: "+rpta)
